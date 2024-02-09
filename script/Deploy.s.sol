22fa1c9694b752ba4157154d3dc51bc6f1424e38
Filip <f@filip.world>
2024-02-08 20:32:12 -0500

    deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,21 +3,34 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {JBArtizenRecoveryTerminal} from "src/JBArtizenRecoveryTerminal.sol";
+import {IJBProjects} from "@juicebox/interfaces/IJBProjects.sol";
+import {IJBDirectory} from "@juicebox/interfaces/IJBDirectory.sol";
 
-// TODO.
 contract CounterScript is Script {
-    function setUp() public {}
-
     function run() public {
+        IJBProjects _projects;
+        IJBDirectory _directory;
+
         uint256 chainId = block.chainid;
+
         if (chainId == 1) {
-            deployArtizenTerminal();
+            // Ethereum mainnet
+            _projects = IJBProjects(0xD8B4359143eda5B2d763E127Ed27c77addBc47d3);
+            _directory = IJBDirectory(0x65572FB928b46f9aDB7cfe5A4c41226F636161ea);
+        } else if (chainId == 5) {
+            // Goerli testnet
+            _projects = IJBProjects(0x21263a042aFE4bAE34F08Bb318056C181bD96D3b);
+            _directory = IJBDirectory(0x8E05bcD2812E1449f0EC3aE24E2C395F533d9A99);            
         } else {
             revert("Invalid chain ID.");
         }
+        
+        vm.startBroadcast();
+        _deployArtizenTerminal(_projects, _directory);
+        vm.stopBroadcast();
     }
     
-    function deployArtizenTerminal() private {
-        new JBArtizenRecoveryTerminal();
+    function _deployArtizenTerminal(IJBProjects _projects, IJBDirectory _directory) private {
+        new JBArtizenRecoveryTerminal(_projects, _directory);
     }
 }

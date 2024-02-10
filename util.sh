f3851ce4689e0b8204d6281e61ebc414697f22b1
Filip <f@filip.world>
2024-02-10 12:45:29 -0500

    deploy goerli + mainnet

--- util.sh
+++ util.sh
@@ -26,7 +26,7 @@ case "$1" in
   "--help") echo "$help_string" ;;
   "deploy:tenderly-fork") source .env && forge script --rpc-url=$RPC_TENDERLY_MAINNET_FORK ./script/Deploy.s.sol:Deploy --broadcast --unlocked --sender $RENE_ADDRESS -vvv ;;
   "script:tenderly-simulate") source .env &&  bash ./simulate.sh ;;
-  "deploy:ethereum-mainnet") source .env && forge script Deploy --chain-id 1 --rpc-url $RPC_ETHEREUM_MAINNET --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY --interactives 1 --sender $SENDER_ETHEREUM_MAINNET -vvv ;;
-  "deploy:ethereum-goerli") source .env && forge script Deploy --chain-id 5 --rpc-url $RPC_ETHEREUM_GOERLI --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY --interactives 1 --sender $SENDER_ETHEREUM_GOERLI -vvv ;;
+  "deploy:ethereum-mainnet") source .env && forge script Deploy --chain-id 1 --rpc-url $RPC_ETHEREUM_MAINNET --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY --ledger --mnemonic-indexes 0 --sender $SENDER_ETHEREUM_MAINNET -vvv ;;
+  "deploy:ethereum-goerli") source .env && forge script Deploy --chain-id 5 --rpc-url $RPC_ETHEREUM_GOERLI --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY --ledger --mnemonic-indexes 0 --sender $SENDER_ETHEREUM_GOERLI -vvv ;;
   *) echo "Invalid command: $1" ;;
 esac
